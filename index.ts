a1de903b3f153cd8317e8162b4a83b11fcb00c79
NataliaMarin490 <marinnatalia10m@gmail.com>
2021-11-15 19:12:05 -0500

    NM Crear-Busqueda en bd

--- index.ts
+++ index.ts
@@ -1,7 +1,33 @@
 import conectarBD from "./db/db";
+import { UserModel } from "./models/user";
+import { Enum_Rol } from "./models/enums";
 
 const main = async () => {
   await conectarBD();
+
+  //Crear un usuario
+    UserModel.create({
+      apellido:"Pamplona",
+      correo:"juanpamplona@gmail.com",
+      identificacion:"4321",
+      nombre:"Juan",
+      rol:Enum_Rol.administrador,
+    })
+      .then((u) => {
+        console.log("usuario creado", u);
+      })
+      .catch((e) => {
+        console.error("Error creando usuario", e);
+      });
+
+  //Obtener usuarios - conssultas
+  await UserModel.find({correo:'marinnatalia10m@gmail.c'})
+    .then((u) => {
+      console.log("usuarios", u);
+    })
+    .catch((e) => {
+      console.error("error obteniendo los usuarios", e);
+    });
 };
 
 main();
